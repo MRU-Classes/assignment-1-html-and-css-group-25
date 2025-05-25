4e6a0b646ef0a267dbd194a827465391000948c8
Gabby Vadnais <gvadn914@mtroyal.ca>
2025-05-24 18:38:55 -0600

    Add files via upload
    I had to combine both mine and Joshua's files together as we couldn't figure out a way to sync our contributions on each file. The planning document describes each contribution made and we both edited everything over call. 
    It also won't let me add a folder so I'm not sure how to put our css in a folder once committed, same with images.

---
+++ Planning Document.lua
@@ -0,0 +1,28 @@
+Planning Document 
+
+Under "Structural Requirements"
+- Joshua created the company logo and found a photo to include on the website - photo from a free image website
+https://www.istockphoto.com/photo/diverse-team-collaborating-in-modern-co-working-space-gm2094337698-565992918?searchscope=image%2Cfilm
+- Joshua ensured appropriate META tags and <title> tags were used on each page 
+- Joshua ensured there was at least one HTML comment
+- Joshua added the external link to MRU website 
+- The one link to another place on the same page we did as the switch between 
+each page (home, FAQ, contact)
+- Gabby created the table 
+- Gabby and Joshua created the consistent menu on each page 
+- Gabby created the form design with all the required elements, and Joshua 
+added the reset and submit buttons at the bottom
+- Gabby checked each file through the validator and ensured there were no errors
+- We both created our HTML and CSS codes referencing class notes and VSCode copilot descriptions when we needed additional information 
+Under "CSS Requirements" 
+- Gabby ensured the current page of the menu was highlighted when you hovered 
+over the menu option
+- Gabby ensured there was use of the pseduo element, pseudo class, contextual selector, 
+and attribute selector on at least one page 
+- Gabby ensure the table has a striped format 
+- Joshua ensured the image floated to the right of a paragraph 
+- Joshua ensured the header and footer were consistent on each page, Gabby added 
+the logo to the header of each page 
+- We both ensured the CSS file was labeled properly and added to the same section 
+as our HTML files 
+- Joshua ensured consistent formatting used on all pages in a CSS file
